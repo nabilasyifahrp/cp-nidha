1905b30e9ad29f149267d68e65c0545e73ca21a3
muturatna <ratnamutumanikam0510@gmail.com>
2025-07-04 09:47:11 +0700

    ngepush hasil buat crud team dan partner dan buat manpower supply

--- app/Http/Controllers/ManPowerController.php
+++ app/Http/Controllers/ManPowerController.php
@@ -8,10 +8,10 @@ class ManPowerController extends Controller
 {
     public function index()
     {
-        // Ambil semua data advantages
+        
         $advantages = Advantage::all();
 
-        // Kirim ke view manpower/index.blade.php
+        
         return view('manpower.index', compact('advantages'));
     }
 }
