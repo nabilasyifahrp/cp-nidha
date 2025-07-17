aee2158fb0caa0092b7eeddf6a292b2e2b18c613
nabilasyifahrp <nabilasyifahrp07@gmail.com>
2025-07-17 15:12:04 +0700

    Merapihkan Tampilan

--- app/Http/Controllers/TrainingCenterController.php
+++ app/Http/Controllers/TrainingCenterController.php
@@ -11,7 +11,7 @@ class TrainingCenterController extends Controller
     public function index()
     {
         $trainings = Training::latest()->take(5)->get();
-        $teams = Team::latest()->take(5)->get();
+        $teams = Team::latest()->take(4)->get();
         return view('layouts.app-training', compact('trainings', 'teams'));
     }
 }
